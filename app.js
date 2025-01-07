896c8671f3de23f2db4002ac71101b1cbb92d8c6
KevinY <tkyDevs@gmail.com>
2025-01-07 11:26:36 -0330

    Added addTask html/css

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ------------------------------------- Mongoose Connection
 const mongoose = require('mongoose');
 const {Task, addTask, getTasks, findTask, updateTask, deleteTask} = require('./models/task');
 
@@ -7,3 +8,27 @@ async function connectMongo() {
 }
 connectMongo().catch(err => console.log(err));
 
+// ------------------------------------- Server Setup
+const express = require('express');
+const app = express();
+app.use(express.static('./public'));
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './index.html'));
+})
+
+app.get('/addTask', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/addTask.html'));
+})
+app.get('/addTask', (req, res) => {
+    res.send('testing');
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('Ops! Looks like you found my invisible border...');
+})
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000...');
+})
